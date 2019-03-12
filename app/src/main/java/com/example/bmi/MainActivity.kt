13a4e65140329af5b2c76b8383df41dffa03263c
Kamil Plich <kamilplich2@gmail.com>
2019-03-12 21:34:11 +0100

    alright, I actually don't know how to check text fields for validity.

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -10,25 +10,6 @@ import com.example.bmi.logic.BMICategory
 import com.example.bmi.logic.BMIFromKgCm
 import kotlinx.android.synthetic.main.activity_main.*
 
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object: TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-            afterTextChanged.invoke(s.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
-    })
-}
-
-fun EditText.validate(validator: (String) -> Boolean, message: String) {
-    this.afterTextChanged {
-        this.error = if (validator(it)) null else message
-    }
-    this.error = if (validator(this.text.toString())) null else message
-}
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bmiObject = BMIFromKgCm()
-
-
 
         countButton.setOnClickListener {
             val mass = Integer.valueOf(massInput.text.toString())
