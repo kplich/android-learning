b24b3dfa8196ca9fcdda0c70824d32ef2dd42222
Kamil Plich <kamilplich2@gmail.com>
2019-03-25 22:29:33 +0100

    implemented handling of out-of-range values

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -16,25 +16,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bmiObject = BMIFromKgCm()
-
         countButton.setOnClickListener {
-            val mass = Integer.valueOf(massInput.text.toString())
-            val height = Integer.valueOf(heightInput.text.toString())
-
-            bmiObject.mass = mass
-            bmiObject.height = height
-
-            val calculatedBMI = BigDecimal(bmiObject.countBMI())
-            val calculatedCategory = bmiObject.getCategory()
-
-            bmiResult.text = String.format(calculatedBMI.setScale(2, BigDecimal.ROUND_HALF_UP).toString())
-            bmiCategory.text = when (calculatedCategory) {
-                BMICategory.UNDERWEIGHT -> getString(R.string.underweight)
-                BMICategory.NORMAL -> getString(R.string.normal)
-                BMICategory.OVERWEIGHT -> getString(R.string.overweight)
-                BMICategory.OBESE -> getString(R.string.obese)
+            // read mass
+            var mass: Int
+            try {
+                mass = massInput.text.toString().toInt()
             }
+            catch (e: NumberFormatException) {
+                massInput.error = getString(R.string.mass_input_error)
+                mass = -1
+            }
+
+            // read height
+            var height: Int
+            try {
+                height = heightInput.text.toString().toInt()
+            }
+            catch (e: NumberFormatException) {
+                heightInput.error = getString(R.string.height_input_error)
+                height = -1
+            }
+
+            // calculate BMI
+            val bmiObject = BMIFromKgCm(mass, height)
+            try {
+                val calculatedBMI = BigDecimal(bmiObject.countBMI())
+
+                val calculatedCategory = bmiObject.getCategory()
+
+                bmiResult.text = String.format(calculatedBMI.setScale(2, BigDecimal.ROUND_HALF_UP).toString())
+                bmiCategory.text = when (calculatedCategory) {
+                    BMICategory.UNDERWEIGHT -> getString(R.string.underweight)
+                    BMICategory.NORMAL -> getString(R.string.normal)
+                    BMICategory.OVERWEIGHT -> getString(R.string.overweight)
+                    BMICategory.OBESE -> getString(R.string.obese)
+                }
+            }
+            catch (e: Exception) {
+                bmiResult.text = ""
+                bmiCategory.text = ""
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/bmi/logic/BMIFromKgCm.kt
+++ app/src/main/java/com/example/bmi/logic/BMIFromKgCm.kt
@@ -1,6 +1,6 @@
 package com.example.bmi.logic
 
-class BMIFromKgCm(var mass: Int = 0, var height: Int = 0): BMI {
+class BMIFromKgCm(val mass: Int = 0, val height: Int = 0): BMI {
     override fun getCategory(): BMICategory {
         return when {
             countBMI() < 18.5 -> BMICategory.UNDERWEIGHT
@@ -11,6 +11,10 @@ class BMIFromKgCm(var mass: Int = 0, var height: Int = 0): BMI {
     }
 
     override fun countBMI(): Double {
+        //TODO: are those lambdas ok?
+        require(mass >= 0, fun(): String { return "Mass must be non-negative" })
+        require(height > 0, fun(): String { return "Height must be greater than 0" })
+
         return mass * 10000.0 / (height * height)
     }
 }
